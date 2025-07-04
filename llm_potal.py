c38044688c56c5b77a54d4f7558268993dcb0bd7
Shyam Kumar <144603220+Shyamkumar682@users.noreply.github.com>
2025-07-04 17:24:17 +0530

    Update llm_potal.py

--- llm_potal.py
+++ llm_potal.py
@@ -266,7 +266,7 @@ def main():
             st.warning("⚠️ Logo not found.")
 
         st.markdown("### 👋 Welcome to GemBharat AI")
-        st.markdown("""<div style="text-align: center; font-size: 17px; margin-top: 10px;">👇 <strong>Click “Continue” to Launch GemBharat AI</strong></div>""", unsafe_allow_html=True)
+        st.markdown("""<div style="text-align: left; font-size: 17px; margin-top: 10px;">👇 <strong>Click “Continue” to Launch GemBharat AI</strong></div>""", unsafe_allow_html=True)
         st.markdown("<div style='margin-top: 25px;'></div>", unsafe_allow_html=True)  # Adds vertical space
        
         if st.button("🚀 Continue", key="continue"):
